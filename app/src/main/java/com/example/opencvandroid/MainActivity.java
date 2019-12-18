56d60a501d10e2e94de4ea0d61c132e7c22c97e5
Saim <saimh355@gmail.com>
2019-12-18 15:04:51 +0500

    Opne CV for Android

--- app/src/main/java/com/example/opencvandroid/MainActivity.java
+++ app/src/main/java/com/example/opencvandroid/MainActivity.java
@@ -3,8 +3,24 @@ package com.example.opencvandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "MainActivity";
+
+    static {
+        if (OpenCVLoader.initDebug())
+        {
+            Log.d(TAG,"Open CV is Configured or Connected Successfully");
+        }
+        else
+        {
+            Log.d(TAG,"Open CV is not working or loaded");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
